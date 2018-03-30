d8b3e272c1bebeb120f57da0984eead974fad039
Alexandre Victoor <alexandre.victoor@fluo.com>
2018-03-30 23:14:48 +0200

    stryker report full width

--- src/slides/3_stryker/stryker_5b.js
+++ src/slides/3_stryker/stryker_5b.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Heading, Notes, Slide, Image } from "spectacle";
-
+// <img src="assets/stryker-report.png" style={{ margin: "-400px" }} />
+ 
 export default (
-    <Slide transition={['zoom']} bgColor="primary">
-                    <Image src="assets/stryker-report.png" />
+    <Slide transition={['zoom']} bgColor="primary" bgImage="assets/stryker-report.png" bgSize="90%" bgRepeat="no-repeat">
                     
                 </Slide>
 
